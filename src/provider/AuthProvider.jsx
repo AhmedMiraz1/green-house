edb94253d2fa805b975e73512336a6868cb964e1
mmirazz <mehedimiraz900@gmail.com>
2024-06-11 21:17:55 +0600

    authProvider added

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,103 @@
+import {
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+  } from "firebase/auth";
+  import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+ 
+//   import useAxiosPublic from "../hooks/useAxiosPublic";
+//   import { removeItem } from "localforage";
+  
+  export const AuthContext = createContext(null);
+ 
+  const googleProvider = new GoogleAuthProvider();
+  
+  const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    console.log(user);
+  
+    // const axiosPublic = useAxiosPublic();
+  
+    // create user
+    const createUser = (email, password) => {
+      setLoading(true);
+      return createUserWithEmailAndPassword(auth, email, password);
+    };
+  
+    // login user
+  
+    const loginUser = (email, password) => {
+      setLoading(true);
+      return signInWithEmailAndPassword(auth, email, password);
+    };
+  
+    //google login
+    const googleLogin = async () => {
+      setLoading(true);
+      const result = await signInWithPopup(auth, googleProvider);
+      return result;
+    };
+  
+    //log Out
+    const logOut = () => {
+      setLoading(true);
+      setUser(null);
+      return signOut(auth);
+    };
+  
+    //update profile
+  
+    const updateUserProfile = (name, photo) => {
+      return updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photo,
+      });
+    };
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        // if (currentUser) {
+        //   const userInfo = {
+        //     email: currentUser.email,
+        //   };
+        //   axiosPublic.post("/jwt", userInfo).then((res) => {
+            
+        //     if (res.data.token) {
+        //       localStorage.setItem("access-token", res.data.token);
+        //       setLoading(false);
+             
+             
+        //     }
+        //   });
+        // } else {
+        //   localStorage.removeItem("access-token");
+        // }
+        setLoading(false);
+      });
+      return () => unsubscribe();
+    }, []);
+  
+    const authInfo = {
+      user,
+      loading,
+      createUser,
+      loginUser,
+      logOut,
+      updateUserProfile,
+      googleLogin,
+    };
+    return (
+      <AuthContext.Provider value={authInfo}>
+        {children}
+      </AuthContext.Provider>
+    );
+  };
+  
+  export default AuthProvider;
