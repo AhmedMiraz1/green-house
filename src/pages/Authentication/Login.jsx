21ded02e820b6046fa54e0f9aa638b8e1877c06c
mmirazz <mehedimiraz900@gmail.com>
2024-06-13 11:38:45 +0600

    login added

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,31 +1,63 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import login from '../../assets/login.jpg'
 import SocialLogin from '../shard/SocialLogin';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
 
 
 const Login = () => {
+  const { loginUser, } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const handelLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    loginUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        Swal.fire({
+          title: "User Login successfully",
+          showClass: {
+            popup: `
+                animate__animated
+                animate__fadeInUp
+                animate__faster
+              `,
+          },
+          hideClass: {
+            popup: `
+                animate__animated
+                animate__fadeOutDown
+                animate__faster
+              `,
+          },
+        });
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        const err = error.message;
+        console.log(err);
+      });
+  };
+
+
   return (
     <div className="mt-12 md:my-40   ">
-      <div className=" w-full md:w-1/2 mx-auto rounded-lg shadow-xl">
-        {/* <div className="w-full">
+      <div className=" flex justify-center rounded-lg shadow-xl">
+        <div className="w-full">
           <img className=' w-full hidden md:flex' src={login} alt="" />
-        </div> */}
+        </div>
         <div className="  w-full   ">
-          <form className="card-body">
+          <form onSubmit={handelLogin} className="card-body">
             <h1 className='text-center font-bold text-4xl text-blue-500'> Welcome Back !</h1>
             <h2 className='text-center font-bold text-3xl my-7 text-blue-500'> Login to your account </h2>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Your Name</span>
-              </label>
-              <input
-                type="name"
-                placeholder="Enter your email"
-                className="input input-bordered"
-                required
-                name="name"
-              />
-            </div>
+           
             <div className="form-control">
               <label className="label">
                 <span className="label-text"> Your Email</span>
@@ -38,18 +70,7 @@ const Login = () => {
                 name="email"
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Your Photo URL </span>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter your email"
-                className="input input-bordered"
-                required
-                name="photo"
-              />
-            </div>
+           
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Your Password</span>
--- src/pages/Authentication/SignUp.jsx
+++ src/pages/Authentication/SignUp.jsx
@@ -1,19 +1,82 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../shard/SocialLogin";
-import signUp from '../../assets/login.jpg'
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+      } = useForm();
+      const {createUser, updateUserProfile,  }=useAuth()
+      const navigate = useNavigate()
+    //   const axiosPublic =useAxiosPublic()
+
+      const onSubmit = (data) => {
+        console.log(data);
+        createUser(data.email, data.password).then(result=> {
+            const user = result.user
+            console.log(user);
+            updateUserProfile(data.name, data.photoURL)
+            .then(()=> {
+            //   const userInfo ={
+            //     name:data.name,
+            //     email: data.email
+            //   }
+            //   axiosPublic.post('/users', userInfo)
+            //   .then(res=> {
+            //     if(res.data.insertedId){
+            //       console.log('user added to the database');
+            //       reset();
+            //       Swal.fire({
+            //           position: "top-end",
+            //           icon: "success",
+            //           title: "User created successfully",
+            //           showConfirmButton: false,
+            //           timer: 1500
+            //         });
+            //         navigate('/')                                
+            //     }
+            //   })
+                
+            })
+    
+            .catch((error)=> {
+                console.log(error.message);
+            })
+        })
+       
+    
+      };
+
     return (
         <div className="mt-12 md:my-40   ">
-      <div className=" flex flex-col  md:flex-row container mx-auto md:px-16 rounded-lg shadow-xl">
-        <div className="w-full">
-          <img className='h-[70vh] w-full hidden md:flex' src={signUp} alt="" />
-        </div>
+      <div className=" w-full md:w-1/2 mx-auto rounded-lg shadow-xl">
+       
         <div className="  w-full  ">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)}  className="card-body">
             <h1 className='text-center font-bold text-4xl text-blue-500'> Welcome Back !</h1>
             <h2 className='text-center font-bold text-3xl my-7 text-blue-500'> Login to your account </h2>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Name</span>
+              </label>
+              <input
+                type="name"
+                placeholder="Enter your email"
+                className="input input-bordered"
+                required
+               
+                {...register("name", { required: true })}
+              />
+               {errors.name && (
+                <span className="text-red-600">Name field is required</span>
+              )}
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -23,8 +86,28 @@ const SignUp = () => {
                 placeholder="Enter your email"
                 className="input input-bordered"
                 required
-                name="email"
+              
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <span className="text-red-600">Email field is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Photo URL </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="input input-bordered"
+                required
+               
+                {...register("photoURL", { required: true })}
+              />
+                {errors.photoURL && (
+                <span className="text-red-600">Name field is required</span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -35,8 +118,33 @@ const SignUp = () => {
                 placeholder="Enter your password"
                 className="input input-bordered"
                 required
-                name="password"
+                {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern:
+                      /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                  })}
               />
+               {errors?.password?.type === "required" && (
+                <span className="text-red-600">Password field is required</span>
+              )}
+              {errors?.password?.type === "minLength" && (
+                <span className="text-red-600">
+                  Password must be 6 characters{" "}
+                </span>
+              )}
+              {errors?.password?.type === "maxLength" && (
+                <span className="text-red-600">
+                  Password must be less then 20 characters{" "}
+                </span>
+              )}
+              {errors?.password?.type === "pattern" && (
+                <span className="text-red-600">
+                  Password must have one Uppercase one lowercase one number and
+                  one special characters
+                </span>
+              )}
             </div>
             <div className="form-control mt-6">
               <input
