8dc38421d57a5953a116cdb22e80f6dbc9274ec7
mmirazz <mehedimiraz900@gmail.com>
2024-06-16 18:42:46 +0600

    handel agreement added

--- src/pages/apartment/ApartmentCard.jsx
+++ src/pages/apartment/ApartmentCard.jsx
@@ -1,7 +1,69 @@
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 
 const ApartmentCard = ({card}) => {
     const {_id, image,apartment_no, block_name, floor_no, rent }=card
+
+
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const location =useLocation()
+    const axiosSecure =useAxiosSecure()
+    // const [, refetch]=useCart()
+  
+    
+  
+    const handelAddToAgreement = () => {
+      if (user && user.email) {
+        // TODO :
+       const cartItem ={
+        menuId :_id,
+        email:user.email,
+        name:user.displayName,
+        floor_no,
+        apartment_no,
+        block_name,
+        rent,
+        status : "pending" 
+       }
+       axiosSecure.post('/agreement', cartItem )
+       .then(res => {
+        console.log(res.data);
+        if(res.data.insertedId){
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: 'Add your apartment ',
+            showConfirmButton: false,
+            timer: 2000
+          });
+        //   refetch();
+        }
+       })
+        
+      } 
+      
+      else {
+        Swal.fire({
+          title: "You are not logged in  ",
+          text: "Please login and add to the cart?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes,Login! ",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // send the user login page
+            navigate("/login", {state:{from:location}} );
+            console.log(navigate);
+          }
+        });
+      }
+    };
     return (
         <div className=" rounded-md shadow-md  text-gray-100">
         <img src={image} alt="" className="object-cover object-center w-full rounded-t-md h-72 " />
@@ -14,7 +76,9 @@ const ApartmentCard = ({card}) => {
                 <h2 className=" "> Floor number : {floor_no}</h2>
                 <p className="">Rent : ${rent}</p>
             </div>
-            <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-blue-500 text-white btn text-lg md:text-xl ">Read more</button>
+            <button 
+            onClick={handelAddToAgreement}
+             type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-blue-500 text-white btn text-lg md:text-xl ">Agreement button</button>
         </div>
     </div>
     );
